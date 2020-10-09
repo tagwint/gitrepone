b0a67c3c1fbd51b3f4bb6266d4e1aa9a00fdb79b
tagwint <tagwint@users.noreply.github.com>
2020-10-09 16:05:39 +0200

    Attempted to adopt to python3, installed python3-xlib, adjusted all print statemnsts faced:     clay=output.replace("\n", "") TypeError: a bytes-like object is required, not 'str'
    Reverrted to python 2 so far untill above resolved

--- home/.i3/i3keepwinlayout.py
+++ home/.i3/i3keepwinlayout.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from Xlib import display, X, Xatom
@@ -23,8 +23,8 @@ class X11WinWatch:
         
     def getKBLayout(self):
         output = subprocess.Popen(["setxkbmap -query | awk ' /layout/ { print $2 }'"], shell=True, stdout=subprocess.PIPE).communicate()[0]
-	clay=output.replace("\n", "")
-	return clay
+        clay=output.replace("\n", "")
+        return clay
 
     def saveKBLayout(self):
         cc=self.getKBLayout()
@@ -36,15 +36,15 @@ class X11WinWatch:
         window = self.display.get_input_focus().focus
         wmname = window.get_wm_name()
         wmclass = wmclass = window.get_wm_class()
-	clay=self.getKBLayout()
-        print "--- Current system layout is ",clay 
+        clay=self.getKBLayout()
+        print ("--- Current system layout is ", clay) 
         if not wmclass is None:
              self.curclass = wmclass[1]
              self.cwlay.setdefault(self.curclass,"us")
-             print "Current is [",clay,"]",self.curclass
+             print("Current is [",clay,"]",self.curclass)
              wlay=self.cwlay[self.curclass]
              self.cwlay[self.curclass] = "us" if not wlay else wlay
-             print "Kept_ here [",wlay,"]",self.curclass
+             print("Kept_ here [",wlay,"]",self.curclass)
              if not wlay == clay:
                  self.cwlay[self.curclass]=clay
                  print("need to switch")
@@ -58,15 +58,16 @@ class X11WinWatch:
         if active != self.activeWindow:
             print('window change detected ...')
             self.activeWindow = active
-	    try:
+            try:
             	self.checkRestoreKBLayout()
             except:
-		print('Exception whne restore KB layout')
+                print('Exception whne restore KB layout')
         
     def run(self):
         self.cwlay.setdefault(self.curclass,"us")
         self.pid=os.getpid()
         with open(self.pidfile, 'w') as f:
+            # print(self.pid, file=f)
             print >>f, self.pid
         while 1:
             #print self.pid,self.curclass, "curclass, its layout is ", self.cwlay[self.curclass]
@@ -79,18 +80,18 @@ class X11WinWatch:
 
 
 
-print "Creating a"
+print("Creating a")
 a=X11WinWatch()
 
 def handler(signum,frame):
-     print "Signal handler caught"
+     print("Signal handler caught")
      a.saveKBLayout()
  
 signal.signal(signal.SIGUSR1, handler)
 
-print "Starting a"
+print("Starting a")
 a.run()
-print "Out of sight"
+print("Out of sight")
 
 ## 
 ## 
